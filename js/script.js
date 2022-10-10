93d6f87692e0d0cd000b3f2c849d127f89aa267c
scurenza <fedechiar@hotmail.it>
2022-10-10 15:27:38 +0200

    problemi con for

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Scrivi un programma che stampi in console i numeri da 1 a 100,
+// ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+// Dati
+let num = 0;
+let fizz = 'Fizz';
+
+// Ciclo iniziale per stampare i numeri in console
+for (let i = 1; i <= 100; i++) {
+
+    if (i % 3 === 0) {
+        i = fizz;
+        
+    }
+    
+    console.log(i);
+    
+}
