9edb2ccb67f77991812d3f2012ed8d0f10760571
scurenza <fedechiar@hotmail.it>
2022-10-10 16:03:33 +0200

    esercizio su console funzionante

--- js/script.js
+++ js/script.js
@@ -11,17 +11,17 @@ let fizzBuzz = 'FizzBuzz';
 // Ciclo iniziale per stampare i numeri in console
 for (let i = 1; i <= 100; i++) {
 
-    if (i % 3 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(fizzBuzz);
+    }
+
+    else if (i % 3 === 0) {
         console.log(fizz);
     }
 
     else if (i % 5 === 0) {
         console.log(buzz);
     }
-
-    // if (i % 3 === 0 && i % 5 === 0) {
-    //     console.log(fizzBuzz);
-    // }
 
     else {
         console.log(i);
